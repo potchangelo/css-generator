ed320819032c1f14f340afe2203b458523e87cc7
Potchara Puttawanchai <demon-angelo@hotmail.com>
2020-02-06 21:39:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
         <App/>
     </BrowserRouter>
 ), document.getElementById('root'));
