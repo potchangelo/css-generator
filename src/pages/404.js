0a9602be27cf668267beaeef86a62050b7f93ea2
Potchara Puttawanchai <potchangelo@gmail.com>
2021-02-17 17:43:38 +0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,17 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default NotFoundPage
+export default () => {
+    return (
+        <section className="section">
+            <div className="container is-max-desktop content">
+                <h1 className="title is-1">404</h1>
+                <h3 className="subtitle is-4">Page not found</h3>
+                <p>The page you requested was not found.</p>
+                <p>
+                    <Link className="button is-link has-text-weight-bold" to="/">Go home</Link>
+                </p>
+            </div>
+        </section>
+    );
+};
