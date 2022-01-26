08679148cc657699de6b70c53dd944ffe1d5c036
Potchara Puttawanchai <potchangelo@gmail.com>
2022-01-26 15:38:50 +0700

    No return brackets for app nav components

--- src/components/_AppNavDimmer.js
+++ src/components/_AppNavDimmer.js
@@ -11,10 +11,8 @@ function _AppNavDimmer() {
       role="button"
       tabIndex={0}
       aria-label="Close navigation"
-      onClick={_ => {
-        navContext.dispatch({ type: 'closeNav' });
-      }}
-      onKeyPress={_ => {}}
+      onClick={_ => { navContext.dispatch({ type: 'closeNav' }) }}
+      onKeyPress={_ => { }}
     />
   );
 }
